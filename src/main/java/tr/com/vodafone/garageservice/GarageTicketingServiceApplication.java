9411ba7090528974d0e085328246751a071efdc5
onur.ciftci <onur.ciftci@yeditepe.edu.tr>
2022-04-22 20:22:52 +0300

    initial commit

---
+++ src/main/java/tr/com/vodafone/garageservice/GarageTicketingServiceApplication.java
@@ -0,0 +1,18 @@
+package tr.com.vodafone.garageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.ZoneId;
+import java.util.TimeZone;
+
+public class GarageTicketingServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GarageTicketingServiceApplication.class, args);
+    }
+
+    @Bean
+    public void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Europe/Istanbul")));
+    }
+}
